205e2891aefce1af66f5a70c4797c3dd88e432a1
David M. Golembiowski <dmgolembiowski@gmail.com>
2019-06-07 12:02:05 -0400

    Create iterating.py

---
+++ Pandas/iterating.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+from pandas import DataFrame
+
+def main(dataFrame):
+    """
+    When working with extremely massive SQL data
+    in Python, nothing stinks more than 
+    iterating when pandas.DataFrame instance methods
+    behave weirdly and steal your syntactic flexibility.
+    
+    Instead of using:
+        pandas.DataFrame.{iterrows,itertuples}
+    convert the container to JSON for a more intutive
+    and readable program.
+    
+    def convert(df: DataFrame) -> list:
+        import json
+        return json.loads(df.to_json(orient='records'))
+    
+    return convert(dataFrame)
+
+            
+            
